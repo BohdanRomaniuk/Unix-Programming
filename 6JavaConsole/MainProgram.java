2b2d4d734ab4979d791d09366e6f47fa7016cfd1
BohdanRomaniuk <bohdan2307@gmail.com>
2019-04-20 06:46:38 -0700

    Simple tests on Java Console

--- 6JavaConsole/MainProgram.java
+++ 6JavaConsole/MainProgram.java
@@ -3,11 +3,15 @@ public class MainProgram
 	public static void RunFirstTaskTests()
 	{
 		System.out.println("Test 1.1: " + (Tests.Test1_1() ? "passed" : "failed"));
+		System.out.println("Test 1.2: " + (Tests.Test1_2() ? "passed" : "failed"));
+		System.out.println("Test 1.3: " + (Tests.Test1_3() ? "passed" : "failed"));
 	}
 	
 	public static void RunSecondTaskTests()
 	{
-		
+		System.out.println("\nTest 2.1: " + (Tests.Test2_1() ? "passed" : "failed"));
+		System.out.println("Test 2.2: " + (Tests.Test2_2() ? "passed" : "failed"));
+		System.out.println("Test 2.3: " + (Tests.Test2_3() ? "passed" : "failed"));
 	}
 	
 	public static void main(String[] args)
