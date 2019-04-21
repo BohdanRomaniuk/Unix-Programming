37b3dc40d225379e8061d6820ca67c34948f33f9
BohdanRomaniuk <bohdan2307@gmail.com>
2019-04-21 00:45:44 -0700

    Remove dublication in 6 Java Console

---
+++ 6JavaConsole/src/tests/TestsJUnit.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import program.Tests;
+
+class TestsJUnit 
+{
+	@Test
+	void Test1_1()
+	{
+		assertTrue(Tests.Test1_1());
+	}
+
+	@Test
+	void Test1_2()
+	{
+		assertTrue(Tests.Test1_2());
+	}
+	
+	@Test
+	void Test1_3()
+	{
+		assertTrue(Tests.Test1_3());
+	}
+	
+	@Test
+	void Test2_1()
+	{
+		assertTrue(Tests.Test2_1());
+	}
+	
+	@Test
+	void Test2_2()
+	{
+		assertTrue(Tests.Test2_2());
+	}
+	
+	@Test
+	void Test2_3()
+	{
+		assertTrue(Tests.Test2_3());
+	}
+}
